8dabc549f1020ab8ca4ae1066e095c98f59842ae
bigjihua <bigjihua@Gmail.com>
2022-12-20 22:40:01 +0800

    NEW

--- JiHuaShortChainSystemAPI/DataBase/LinkDataBase.js
+++ JiHuaShortChainSystemAPI/DataBase/LinkDataBase.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const linkDataBase = mysql.createPool({
-    host:'',
-    user:'',
-    password:'',
-    database:''
+    host:'43.129.172.233',
+    user:'ShortChain',
+    password:'ShortChain',
+    database:'ShortChain'
 })
 
 module.exports = linkDataBase
