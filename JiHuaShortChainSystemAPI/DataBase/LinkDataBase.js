a0b28712a37a91256357d1cbec94d21beb184bca
bigjihua <bigjihua@Gmail.com>
2023-05-16 17:01:20 +0800

    2023年5月16日修改

--- JiHuaShortChainSystemAPI/DataBase/LinkDataBase.js
+++ JiHuaShortChainSystemAPI/DataBase/LinkDataBase.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const linkDataBase = mysql.createPool({
-    host: '47.115.225.101',
+    host: '',
     user:'ShortChain',
     password:'ShortChain',
     database:'ShortChain'
